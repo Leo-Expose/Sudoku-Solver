eff154179212f7430e75c13b051777b6d8b66ab1
Starlight <88476286+Leo-Expose@users.noreply.github.com>
2024-06-09 15:08:14 +0530

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,26 +1,35 @@
 @echo off
 echo Checking if Flask is installed
+timeout /t 2 >nul
+
 python -c "import flask" 2>nul
 if %errorlevel% neq 0 (
     echo Flask is not installed. Installing Flask...
     pip install flask
+    timeout /t 2 >nul
 )
 
-echo Start the Flask server
+echo Starting the server
+timeout /t 2 >nul
+
 start "" /b cmd /c "python solver.py"
 
 echo Wait for the server to start
-  
+timeout /t 2 >nul
+
 echo Waiting for the server to start...
 :waitloop
 curl -s http://127.0.0.1:5000 >nul
 if %errorlevel% neq 0 (
-    timeout /t 1 >nul
+    timeout /t 2 >nul
     goto waitloop
 )
 
 echo Opening the browser
+timeout /t 2 >nul
+
 start http://127.0.0.1:5000
 echo Server is up & running.
-  
+timeout /t 2 >nul
+
 exit
